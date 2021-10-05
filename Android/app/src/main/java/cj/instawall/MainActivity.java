c23a8897df8ff5e3a6edc572a828924fce63baba
chinmayji <chinmayjain08@gmail.com>
2021-10-05 21:49:45 +0530

    resize

--- Android/app/src/main/java/cj/instawall/MainActivity.java
+++ Android/app/src/main/java/cj/instawall/MainActivity.java
@@ -8,11 +8,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.webkit.ConsoleMessage;
@@ -49,8 +52,8 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     final String TAG = "CJ";
-    String HWV_FLAG="",HWV_LAST_VISIT="", POST_DOWNLOAD_ACTION="";
-    String get_all_links,insert_dwn_btns,download_post;
+    String HWV_FLAG = "", HWV_LAST_VISIT = "", POST_DOWNLOAD_ACTION = "";
+    String get_all_links, insert_dwn_btns, download_post;
     int DOWNLOAD_STACK_COUNT = 0;
     String username = "chinmayjain08";
     Random rnd = new Random();
@@ -58,10 +61,11 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> recent_downloads = new ArrayList<>();
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor spEditor;
-    WebView wv,hwv;
+    WebView wv, hwv;
     WebChromeClient wvChromeClient;
     WebViewClient wvClient;
-    Button run,hide_wv,sync,random;
+    Button run, hide_wv, sync, random;
+
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,19 +89,19 @@ public class MainActivity extends AppCompatActivity {
         setupHWV();
 
         wv.loadUrl("https://www.instagram.com/chinmayjain08/saved");
-        
+
         run.setOnClickListener(view -> {
             Log.d(TAG, String.valueOf(url_to_name.keySet().size()));
             Log.d(TAG, Arrays.toString(url_to_name.get(HWV_LAST_VISIT).toArray()));
             Log.d(TAG, Arrays.toString(recent_downloads.toArray()));
         });
         run.setOnLongClickListener(view -> {
-            saveObject(url_to_name,"url_to_name_backup");
+            saveObject(url_to_name, "url_to_name_backup");
             Log.d(TAG, "Backedup");
             return true;
         });
         hide_wv.setOnClickListener(view -> {
-            if(wv.getVisibility() == View.VISIBLE) wv.setVisibility(View.INVISIBLE);
+            if (wv.getVisibility() == View.VISIBLE) wv.setVisibility(View.INVISIBLE);
             else wv.setVisibility(View.VISIBLE);
         });
         sync.setOnClickListener(view -> {
@@ -115,36 +119,38 @@ public class MainActivity extends AppCompatActivity {
             random.setText("");
             random.setClickable(false);
         });
+
+        // --------------------
+
+//        setWallpaper("197849760_234294944779150_2759185063273606450_n.jpg");
+
+        // --------------------
     }
 
-    void saveObject(Object o, String n){
+    void saveObject(Object o, String n) {
         try {
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(new File(getFilesDir(),n)));
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(new File(getFilesDir(), n)));
             out.writeObject(o);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             Log.d(TAG, e.toString());
         }
     }
 
-    void readObjects(){
+    void readObjects() {
         try {
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream(new File(getFilesDir(),"url_to_name")));
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream(new File(getFilesDir(), "url_to_name")));
             url_to_name = (HashMap<String, HashSet<String>>) in.readObject();
-        }
-        catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             Log.d(TAG, "FileNotFound");
             url_to_name = new HashMap<>();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             Log.d(TAG, Log.getStackTraceString(e));
         }
-        if(url_to_name == null || url_to_name.keySet().size() == 0){
+        if (url_to_name == null || url_to_name.keySet().size() == 0) {
             try {
-                ObjectInputStream in = new ObjectInputStream(new FileInputStream(new File(getFilesDir(),"url_to_name_backup")));
+                ObjectInputStream in = new ObjectInputStream(new FileInputStream(new File(getFilesDir(), "url_to_name_backup")));
                 url_to_name = (HashMap<String, HashSet<String>>) in.readObject();
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 Log.d(TAG, "errrrrrr");
             }
         }
@@ -152,7 +158,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    void readScripts(){
+    void readScripts() {
         try {
             Scanner s = new Scanner(getAssets().open("scripts/get_all_links.js")).useDelimiter("\\A");
             get_all_links = s.hasNext() ? s.next() : "";
@@ -166,69 +172,69 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    void setupHWV(){
+    void setupHWV() {
         hwv.getSettings().setJavaScriptEnabled(true);
         hwv.getSettings().setDomStorageEnabled(true);
         hwv.setWebViewClient(wvClient);
         hwv.setWebChromeClient(wvChromeClient);
     }
 
-    void setupWV(){
+    void setupWV() {
         wv.getSettings().setJavaScriptEnabled(true);
         wv.getSettings().setDomStorageEnabled(true);
-        wvClient = new WebViewClient(){
+        wvClient = new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                if(view == wv) {
+                if (view == wv) {
 //                    if(wv.getUrl().equals("http://i"))
                     Log.d(TAG, wv.getUrl());
                     wv.evaluateJavascript(insert_dwn_btns, null);
                 }
-                if(view == hwv){
-                    if(HWV_FLAG.equals("sync")){
+                if (view == hwv) {
+                    if (HWV_FLAG.equals("sync")) {
                         HWV_FLAG = "";
-                        hwv.evaluateJavascript(get_all_links,null);
-                    }
-                    else if(HWV_FLAG.equals("download_post")){
+                        hwv.evaluateJavascript(get_all_links, null);
+                    } else if (HWV_FLAG.equals("download_post")) {
                         Log.d(TAG, "download_post");
                         HWV_FLAG = "";
-                        hwv.evaluateJavascript(download_post,null);
+                        hwv.evaluateJavascript(download_post, null);
                     }
                 }
                 super.onPageFinished(view, url);
             }
         };
 
-        wvChromeClient = new WebChromeClient(){
+        wvChromeClient = new WebChromeClient() {
             @Override
             public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
                 String action = "";
                 String mess = consoleMessage.message();
-                for (int i=0;i<mess.length();i++){
-                    if(mess.charAt(i)==':'){
-                        mess = mess.substring(i+1);
+                for (int i = 0; i < mess.length(); i++) {
+                    if (mess.charAt(i) == ':') {
+                        mess = mess.substring(i + 1);
                         break;
                     }
-                    action+=mess.charAt(i);
+                    action += mess.charAt(i);
                 }
-                if(action.equals("flag_hwv")){
+                if (action.equals("flag_hwv")) {
                     HWV_FLAG = mess;
                 }
-                if(action.equals("post_link")){
+                if (action.equals("post_link")) {
                     url_to_name.put(mess, new HashSet<>());
                 }
-                if(action.equals("visit")){
+                if (action.equals("visit")) {
                     HWV_LAST_VISIT = mess;
-                    hwv.loadUrl("http://instagram.com"+mess);
+                    hwv.loadUrl("http://instagram.com" + mess);
                 }
-                if(action.equals("download_cnt")){
+                if (action.equals("download_cnt")) {
                     DOWNLOAD_STACK_COUNT = Integer.parseInt(mess);
                 }
-                if(action.equals("download")){
+                if (action.equals("download")) {
                     downloadFile(mess);
                 }
                 Log.d(TAG, action + " : " + mess);
-                if(action.isEmpty())Log.d(TAG, "onConsoleMessage: " + consoleMessage.message() + " -- From line " + consoleMessage.lineNumber() + " of " + consoleMessage.sourceId());
+                if (action.isEmpty())
+                    Log.d(TAG, "onConsoleMessage: " + consoleMessage.message() + " -- From line " + consoleMessage.lineNumber() + " of " + consoleMessage.sourceId());
                 return super.onConsoleMessage(consoleMessage);
             }
         };
@@ -244,61 +250,71 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onPause() {
-        saveObject(url_to_name,"url_to_name");
+        saveObject(url_to_name, "url_to_name");
         super.onPause();
     }
 
     @Override
     public void onBackPressed() {
-        if(wv!=null&&wv.canGoBack()){
+        if (wv != null && wv.canGoBack()) {
             wv.goBack();
-        }
-        else{
+        } else {
             super.onBackPressed();
         }
     }
-    String filenameFromUrl(String url){
+
+    String filenameFromUrl(String url) {
         Pattern p = Pattern.compile("\\w*.jpg");
         Matcher m = p.matcher(url);
-        if(m.find()){
-            return url.substring(m.start(),m.end());
+        if (m.find()) {
+            return url.substring(m.start(), m.end());
         }
         return "none";
     }
 
-    void setWallpaper(String fnm){
+    void setWallpaper(String fnm) {
         try {
-            Bitmap bitmap = BitmapFactory.decodeFile(getExternalFilesDir(null).getAbsolutePath()+"/"+fnm);
+            Bitmap bitmap = BitmapFactory.decodeFile(getExternalFilesDir(null).getAbsolutePath() + "/" + fnm);
+            DisplayMetrics met = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(met);
+            int w = met.widthPixels;
+            int h = met.heightPixels;
+            Bitmap background = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(background);
+            bitmap = Bitmap.createScaledBitmap(bitmap, w, (int)((float)w/(float)bitmap.getWidth()*(float)bitmap.getHeight()), true);
+
+            canvas.drawBitmap(bitmap,0,(h-bitmap.getHeight())/2, new Paint());
+
+            background.compress(Bitmap.CompressFormat.PNG, 100, new FileOutputStream(new File(getExternalFilesDir(null),"test.png")));
+            Log.d(TAG, String.valueOf(w) + " " + String.valueOf(h));
             WallpaperManager manager = WallpaperManager.getInstance(getApplicationContext());
-            manager.setBitmap(bitmap);
+            manager.setBitmap(background);
             Log.d(TAG, "Wallpaper set");
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             Log.d(TAG, Log.getStackTraceString(e));
         }
-        random.post(()->{
+        random.post(() -> {
             random.setClickable(true);
             random.setText("W");
             Toast.makeText(getApplicationContext(), "Wallpaper Changed", Toast.LENGTH_SHORT).show();
         });
     }
 
-    void downloadFile(String url){
+    void downloadFile(String url) {
         String filename = filenameFromUrl(url);
         Log.d(TAG, filename);
         new Thread(() -> {
-            try{
-                File f = new File(getExternalFilesDir(null),filename);
-                if(f.exists()){
+            try {
+                File f = new File(getExternalFilesDir(null), filename);
+                if (f.exists()) {
                     Log.d(TAG, "already exists");
-                }
-                else{
+                } else {
                     BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
                     FileOutputStream out = new FileOutputStream(f);
                     byte[] buffer = new byte[1024];
                     int rd;
-                    while((rd=in.read(buffer))>0){
-                        out.write(buffer,0,rd);
+                    while ((rd = in.read(buffer)) > 0) {
+                        out.write(buffer, 0, rd);
                     }
                     out.close();
                     in.close();
@@ -307,13 +323,12 @@ public class MainActivity extends AppCompatActivity {
                 url_to_name.get(HWV_LAST_VISIT).add(filename);
                 recent_downloads.add(filename);
 
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 Log.d(TAG, Log.getStackTraceString(e));
             }
             DOWNLOAD_STACK_COUNT--;
-            if(POST_DOWNLOAD_ACTION.equals("wallpaper")){
-                if(DOWNLOAD_STACK_COUNT == 0){
+            if (POST_DOWNLOAD_ACTION.equals("wallpaper")) {
+                if (DOWNLOAD_STACK_COUNT == 0) {
                     int ri = rnd.nextInt(recent_downloads.size());
                     Log.d(TAG, "Set wallpaper" + recent_downloads.get(ri));
                     setWallpaper(recent_downloads.get(ri));
